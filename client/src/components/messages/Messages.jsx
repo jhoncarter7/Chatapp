168930c79d2f3c0a1412ff224c28e2fce3d90646
jhoncarter7 <rainikhil044@gmail.com>
2024-04-05 20:34:46 +0530

    implement search functionality

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import useGetMessage from "../../Hooks/useGetMessage";
 import MessageSkeleton from "../skeleton/MessageSkeleton";
 import Message from "./Message";
@@ -5,15 +6,28 @@ import Message from "./Message";
 const Messages = () => {
   const { loading, messages } = useGetMessage();
 
+  const lastMessageRef = useRef();
+  useEffect(() => {
+      setTimeout(()=> {
+        lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+      }, 1000)
+  
+  }, [messages])
+
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {!loading && messages.length > 0 && messages?.map((message)=> <Message key={message._id} message={message}/>)}
+      {!loading &&
+        messages.length > 0 &&
+        messages?.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
       {loading &&
         [...Array(3)].map((_, indx) => <MessageSkeleton key={indx} />)}
-        {
-          !loading && messages.length === 0 && ( <p className="text-center text-gray-400">No messages yet</p>)
-        }
-
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-gray-400">No messages yet</p>
+      )}
     </div>
   );
 };
--- client/src/components/sidebar/Searchinput.jsx
+++ client/src/components/sidebar/Searchinput.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
-
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../Hooks/useGetConversations";
+import { toast } from "react-hot-toast";
 const Searchinput = () => {
-  return (
-    <form className="flex items-center gap-2">
-        <input type="search" placeholder="search..." className="input input-bordered rounded-full h-10" />
-        <button type="submit" className="btn btn-circle bg-sky-500 text-white">
-            <AiOutlineSearch className="w-6 h-6 outline-none"/>
-        </button>
-    </form>
-  )
-}
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
 
-export default Searchinput
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!search) return;
+
+    if (search.length < 3)
+      toast.error("Search query must be at least 3 characters long");
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      toast.error("No user found");
+    }
+  };
+  return (
+    <form onSubmit={submitHandler} className="flex items-center gap-2">
+      <input
+        type="search"
+        placeholder="search..."
+        className="input input-bordered rounded-full h-10"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <AiOutlineSearch className="w-6 h-6 outline-none" />
+      </button>
+    </form>
+  );
+};
+
+export default Searchinput;
