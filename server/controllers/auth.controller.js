31bf321c4ce13526a7dccd8ee1b6e91a2ce56af8
jhoncarter7 <rainikhil044@gmail.com>
2024-04-17 18:26:52 +0530

    Update authentication controller to include cookie options

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -51,62 +51,78 @@ const signUpUser = async function (req, res, next) {
         "Something went wrong while registering the user"
       );
     }
-
+    const accessToken = await createdUser.generateAccessToken();
+    
+    const options = {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 15 * 24 * 60 * 60 * 1000,
+    };
     return res
       .status(201)
+      .cookie("accessToken", accessToken, options)
       .json(new ApiResponse(200, createdUser, "User successfully created"));
   } catch (error) {
-   res.status(500).json({error: error.message})
+    res.status(500).json({ error: error.message });
   }
 };
 
 const loginUser = async (req, res) => {
   try {
-    
- 
-  const { userName, email, password } = req.body;
+    const { userName, email, password } = req.body;
 
-  if (!(userName || email)) {
-    throw new ApiError(400, "All fields are required");
+    if (!(userName || email)) {
+      throw new ApiError(400, "All fields are required");
+    }
+
+    const userExist = await User.findOne({
+      $or: [{ userName }, { email }],
+    });
+
+    if (!userExist) {
+      throw new ApiError(404, "User does not exist");
+    }
+    const correctPassword = await userExist.isPasswordCorrect(password);
+
+    if (!correctPassword) {
+      throw new ApiError(401, "Invalid credentials");
+    }
+
+    const accessToken = await userExist.generateAccessToken();
+
+    const loggedinUser = await User.findById(userExist._id).select("-password");
+
+    const options = {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 15 * 24 * 60 * 60 * 1000,
+    };
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { user: loggedinUser, accessToken },
+          "user is successfully loggedin"
+        )
+      );
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
   }
-
-  const userExist = await User.findOne({
-    $or: [{ userName }, { email }],
-  });
-
-  if (!userExist) {
-    throw new ApiError(404, "User does not exist");
-  }
-  const correctPassword = await userExist.isPasswordCorrect(password);
-
-  if (!correctPassword) {
-    throw new ApiError(401, "Invalid credentials");
-  }
-
-  const accessToken = await userExist.generateAccessToken();
-   
-  const loggedinUser = await User.findById(userExist._id).select("-password");
-
-  const options = {
-    httpOnly: true,
-    sameSite: "Strict",
-    secure: process.env.NODE_ENV !== "development",
-    maxAge: 15 * 24 * 60 * 60 * 1000,
-
-  };
-  return res.status(200).cookie("accessToken", accessToken, options).json( new ApiResponse(200, {user: loggedinUser, accessToken}, "user is successfully loggedin" ));
-} catch (error) {
-  console.log(error.message);
-  res.status(500).json({error: error.message})
-}
 };
 
 const logoutUser = async (req, res) => {
-try {
-  return res.clearCookie("accessToken").json(new ApiResponse(200, {}, "User successfully logged out"));
-} catch (error) {
-  res.status(500).json({error: error.message})
-}
+  try {
+    return res
+      .clearCookie("accessToken")
+      .json(new ApiResponse(200, {}, "User successfully logged out"));
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export { loginUser, signUpUser, logoutUser };
